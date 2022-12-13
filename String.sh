4dcccda4dbc2d80071daf4a13a89d7f832fbb550
TestDevil <120416475+SavageDevil15@users.noreply.github.com>
2022-12-13 14:08:07 +0530

    Add files via upload

--- String.sh
+++ String.sh
@@ -1,5 +1,19 @@
+str=$1
+length_str=${#str}
+substr=$2
+length_substr=${#substr}
+count=0
+
+for ((  i=0; i<length_str; i++ ))
+do
+    temp=${str:i:length_substr}
+    if [ $temp == $substr ]
+    then
+        count=$((count+1))
+    fi
+done
+
+
 echo "String = $1"
 echo "Sub-string = $2"
-
-echo $1 | grep -o "$2" | wc -l
-
+echo "Count = $count"
